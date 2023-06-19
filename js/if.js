eb0b45a139139356e73fb9d44eb2552703bb1537
Yajayra-08 <136999782+Yajayra-08@users.noreply.github.com>
2023-06-18 20:06:35 -0900

    Add files via upload

---
+++ js/if.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    const header = document.getElementById('header');
+    if (window.pageYOffset > 50) {
+      header.classList.add('fixed');
+    } else {
+      header.classList.remove('fixed');
+    }
+  });
